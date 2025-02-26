ec0501ad04e943cf868aa80a8d1eb3ec4b70be0b
shaikhmoid <moidshaikh313@gmail.com>
2025-02-26 11:40:10 +0530

    third commit

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="red">helpppp</div>;
+  return <div className="">helpppp</div>;
 }
--- src/lib/DB/mongo.db.js
+++ src/lib/DB/mongo.db.js
@@ -24,5 +24,7 @@ export const connectToDatabase = async () => {
 
   cached.conn = await cached.promise;
 
+  console.log("databese connected");
+
   return cached.conn;
 };
